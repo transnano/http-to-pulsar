9f1083b4e490df0413a85281919d57b0ac11d75f
Transnano <transnano.jp@gmail.com>
2020-09-16 23:02:11 +0900

    :package: Add prometheus exporter.

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/transnano/http-to-pulsar
 
 go 1.15
+
+require (
+	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/common v0.13.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,23 +4,111 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/common/version"
 )
 
-func testEndpoint(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	time.Sleep(5 * time.Second)
-	if _, err := w.Write([]byte("ok")); err != nil {
-		log.Fatalf("testEndpoint: %s\n", err)
+const (
+	namespace = "http2pulsar" // For Prometheus metrics.
+)
+
+type Exporter struct {
+	mutex        sync.Mutex
+	receivedSns  prometheus.Counter
+	sendSns      prometheus.Counter
+	failedSns    prometheus.Counter
+	sentDuration prometheus.Gauge
+}
+
+func NewExporter() *Exporter {
+	return &Exporter{
+		receivedSns: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "received_sns_total",
+			Help:      "Total number of received sns.",
+		}),
+		sendSns: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "sent_sns_total",
+			Help:      "Total number of processed sns sent to pulsar broker.",
+		}),
+		failedSns: prometheus.NewCounter(prometheus.CounterOpts{
+			Namespace: namespace,
+			Name:      "failed_sns_total",
+			Help:      "Total number of processed sns which failed on send to pulsar broker.",
+		}),
+		sentDuration: prometheus.NewGauge(
+			prometheus.GaugeOpts{
+				Name: "sent_sns_duration_seconds",
+				Help: "Duration of sns send calls to the pulsar broker.",
+			},
+		),
 	}
 }
 
+func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
+	e.receivedSns.Describe(ch)
+	e.sendSns.Describe(ch)
+	e.failedSns.Describe(ch)
+	e.sentDuration.Describe(ch)
+}
+
+func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
+	e.mutex.Lock() // To protect metrics from concurrent collects.
+	defer e.mutex.Unlock()
+	e.receivedSns.Collect(ch)
+	e.sendSns.Collect(ch)
+	e.failedSns.Collect(ch)
+	e.sentDuration.Collect(ch)
+}
+
+func sendMessage(e *Exporter) {
+	begin := time.Now()
+	time.Sleep(5 * time.Second)
+	duration := time.Since(begin).Seconds()
+	e.sendSns.Inc()
+	e.sentDuration.Set(duration)
+	// if err != nil {
+	// 	e.failedSns.Inc()
+	// 	return err
+	// }
+}
+
 func main() {
+	exporterName := namespace + "_exporter"
+	exporter := NewExporter()
+	prometheus.MustRegister(exporter)
+	prometheus.MustRegister(version.NewCollector(exporterName))
+	// Add Go module build info.
+	prometheus.MustRegister(prometheus.NewBuildInfoCollector())
+
 	mux := http.NewServeMux()
-	mux.HandleFunc("/test", testEndpoint)
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		sendMessage(exporter)
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write([]byte("ok")); err != nil {
+			log.Fatalf("testEndpoint: %s\n", err)
+		}
+	})
+
+	disablePprof := false
+	// Enable pprof endpoint if enabled by cmdline
+	if !disablePprof {
+		mux.HandleFunc("/debug/pprof/", pprof.Index)
+		mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+	}
 
 	srv := &http.Server{
 		Addr:    ":8080",
